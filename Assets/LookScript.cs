36f37f1d2d72513c4f9a47feba549d03174c4c7e
dipesh paneru <dipeshpaneru1997@gmail.com>
2024-11-12 18:28:25 -0500

    Player camera movements added

--- Assets/LookScript.cs
+++ Assets/LookScript.cs
@@ -1,16 +1,28 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class LookScript : MonoBehaviour
 {
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    public Transform playerbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerbody.Rotate(Vector3.up * mouseX);
     }
 }
