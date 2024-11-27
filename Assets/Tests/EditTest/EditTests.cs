eaabd8d1b5364329b27f25279051aefc51ff3795
dipesh paneru <dipeshpaneru1997@gmail.com>
2024-11-27 17:24:07 -0500

    Tests added

---
+++ Assets/Tests/EditTest/EditTests.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class DirectionTests
+{
+    
+    [Test]
+    public void XYPositionTest()
+    {
+      Assert.AreEqual(1, 1);
+    }
+}
+
+
+public class CameraLookTest
+{
+    
+    [Test]
+    public void XYPositionTest()
+    {
+      Assert.AreEqual(1, 1);
+    }
+}
+
